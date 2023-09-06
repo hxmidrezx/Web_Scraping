6048a23c4ab87fd333ebaa44a8a8c5c240184df5
HamidReza Farahani <hxmidrezx@HamidRezas-MacBook-Pro.local>
2023-09-06 19:09:56 -0400

    add images

--- books.py
+++ books.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
+from urllib.parse import urljoin
+
 url = "http://books.toscrape.com/catalogue/page-{}.html"
 
 def fetch_site(url):
     response = requests.get(url)
     if response.status_code == 200:
-        return response.text
+        return response.content
     return None
 
 def extract_book_details(content):
@@ -27,16 +30,29 @@ def extract_book_details(content):
         all_books.append(dict_book)
     return all_books
 
+def extract_img(content, url, i):
+    soup = BeautifulSoup(content, "html.parser")
+    images = soup.find_all("img")
+    os.makedirs(f"all_images/page_{i}_images", exist_ok = True)
+    for img in images:
+        src = img["src"]
+        img_url = urljoin(url, src)
+        img_name = os.path.basename(src)
+        img_data = fetch_site(img_url)
+        with open(os.path.join(f"all_images/page_{i}_images", img_name),"wb")as f :
+            f.write(img_data)
 def save_to_csv(all_books, file_name):
     df = pd.DataFrame(all_books)
     df.to_csv(file_name)
 
 books = []
+os.makedirs("all_images", exist_ok = True)
 i = 1
 while True :
     response = fetch_site(url.format(i))
     print(url.format(i))
     if response :
+        extract_img(response, url, i)
         all_books = extract_book_details(response)
         books = books + all_books
         i += 1
